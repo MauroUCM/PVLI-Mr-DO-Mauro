79b9963c024c0ac5969f28c5b77b942b9a25f144
MauroUCM <maumar03@ucm.es>
2026-06-03 15:26:41 +0200

    spritesheets

--- src/Scenes/boot.js
+++ src/Scenes/boot.js
@@ -11,8 +11,8 @@ export default class Boot extends Phaser.Scene {
 		this.load.image('title', 'assets/UI/mainTitle.png');
 		this.load.image('scroll_pattern', 'assets/UI/titleScrollPattern.png');
 		
-		// this.load.spritesheet('penguin', 'assets/penguin40.png', {frameWidth: 40, frameHeight: 40});
-		// this.load.spritesheet('rat', 'assets/rat32.png', {frameWidth: 32, frameHeight: 32});
+		this.load.spritesheet('mr_do', 'assets/characters/mr_do_spritesheet.png', {frameWidth: 16, frameHeight: 16});
+		this.load.spritesheet('mr_do', 'assets/characters/mr_do_spritesheet.png', {frameWidth: 16, frameHeight: 16});
 	}
 
 	// Creación de la escena.
